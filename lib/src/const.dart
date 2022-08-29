839ea4e4a14c6a00d7c1e9a16ffad8b691e289c2
krijk <kenvanrijk@gmail.com>
2022-08-29 12:17:26 +0900

    add a const text

--- lib/src/const.dart
+++ lib/src/const.dart
@@ -1,12 +1,15 @@
 /// Library constants
 class Const {
   /// utility text short length
-  static String myTextShort =
-      'Lorem ipsum dolor sit amet';
+  static String myTextShort = 'Lorem ipsum dolor sit amet';
 
   /// utility text mid length
   static String myTextMid =
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
+
+  /// utility text long length
+  static String myTextLong =
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 
   /// Android navigation bar default height
 // FIXME: should be retrieved dynamically
