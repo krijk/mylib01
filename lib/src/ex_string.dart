73b571128b281d5baa03d957633d48656d219581
krijk <kenvanrijk@gmail.com>
2022-10-09 08:29:22 +0900

    add extensions for Japanese characters

--- lib/src/ex_string.dart
+++ lib/src/ex_string.dart
@@ -5,3 +5,28 @@ extension StringExtension on String {
     return '${this[0].toUpperCase()}${substring(1)}';
   }
 }
+
+const int _japaneseFullLengthCode = 65248;
+
+/// handling Japanese characters
+extension JapaneseString on String {
+  /// Convert alphanumeric to full length characters
+  String alphanumericToFullLength() {
+    final RegExp regex = RegExp(r'^[a-zA-Z0-9]+$');
+    final Iterable<String> string = runes.map<String>((int rune) {
+      final String char = String.fromCharCode(rune);
+      return regex.hasMatch(char) ? String.fromCharCode(rune + _japaneseFullLengthCode) : char;
+    });
+    return string.join();
+  }
+
+  /// Convert full length to alphanumeric characters
+  String alphanumericToHalfLength() {
+    final RegExp regex = RegExp(r'^[Ａ-Ｚａ-ｚ０-９]+$');
+    final Iterable<String> string = runes.map<String>((int rune) {
+      final String char = String.fromCharCode(rune);
+      return regex.hasMatch(char) ? String.fromCharCode(rune - _japaneseFullLengthCode) : char;
+    });
+    return string.join();
+  }
+}
