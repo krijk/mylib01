c96aa1c1a6c3eb6a810a9a29224f875964922bf8
krijk <kenvanrijk@gmail.com>
2024-06-25 16:19:08 +0900

    code cleanup

--- lib/src/ex_string.dart
+++ lib/src/ex_string.dart
@@ -6,26 +6,28 @@ extension StringExtension on String {
   }
 }
 
-const int _japaneseFullLengthCode = 65248;
+// https://zenn.dev/clay/articles/39208e1f8d442c
+// The only relationship between full-width and half-width is to add or subtract 65248
+const int _diffHanZen = 65248;
 
 /// handling Japanese characters
 extension JapaneseString on String {
   /// Convert alphanumeric to full length characters
-  String alphanumericToFullLength() {
+  String alphanumericToFullWidth() {
     final RegExp regex = RegExp(r'^[a-zA-Z0-9]+$');
     final Iterable<String> string = runes.map<String>((int rune) {
       final String char = String.fromCharCode(rune);
-      return regex.hasMatch(char) ? String.fromCharCode(rune + _japaneseFullLengthCode) : char;
+      return regex.hasMatch(char) ? String.fromCharCode(rune + _diffHanZen) : char;
     });
     return string.join();
   }
 
-  /// Convert full length to alphanumeric characters
-  String alphanumericToHalfLength() {
+  /// Convert numbers and alphabets to half-width characters
+  String alphanumericToHalfWidth() {
     final RegExp regex = RegExp(r'^[Ａ-Ｚａ-ｚ０-９]+$');
     final Iterable<String> string = runes.map<String>((int rune) {
       final String char = String.fromCharCode(rune);
-      return regex.hasMatch(char) ? String.fromCharCode(rune - _japaneseFullLengthCode) : char;
+      return regex.hasMatch(char) ? String.fromCharCode(rune - _diffHanZen) : char;
     });
     return string.join();
   }
